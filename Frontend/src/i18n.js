721ec8c4f24cda06761dab5b47d725847211de0e
Stelio Guerra <stelio.guerra@gmail.com>
2024-03-22 02:21:54 +0100

    Feat: getting started with translation

--- Frontend/src/i18n.js
+++ Frontend/src/i18n.js
@@ -1,28 +1,19 @@
 import i18n from "i18next";
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 
 i18n
     // detect user language
     .use(LanguageDetector)
     // pass the i18n instance to react-i18next.
     .use(initReactI18next)
+    .use(Backend)
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
         debug: true,
-        fallbackLng: 'en',
-        resources: {
-            en: {
-                translation: {}
-            },
-            fr: {
-                translation: {}
-            },
-            it: {
-                translation: {}
-            }
-        }
+        fallbackLng: 'en'
     });
 
 export default i18n;
